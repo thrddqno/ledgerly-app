4601863dcda8b8be99d7252718d91dae60649573
thrddqno <antonio.dioquino@hotmail.com>
2026-02-02 17:18:42 +0800

    Initial Commit

---
+++ backend/ledgerly-api/src/test/java/com/ledgerly/ledgerly/LedgerlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledgerly.ledgerly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
