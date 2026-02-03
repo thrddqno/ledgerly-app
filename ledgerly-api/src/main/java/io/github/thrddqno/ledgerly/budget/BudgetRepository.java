be36b875b6ef904427311dbe9514f0d387f136cd
thrddqno <antonio.dioquino@hotmail.com>
2026-02-03 23:16:21 +0800

    fix type in generics replaced with budget class instead of its repository

--- ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/budget/BudgetRepository.java
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/budget/BudgetRepository.java
@@ -2,6 +2,6 @@ package io.github.thrddqno.ledgerly.budget;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BudgetRepository extends JpaRepository<BudgetRepository, Integer> {
+public interface BudgetRepository extends JpaRepository<Budget, Integer> {
 
 }
