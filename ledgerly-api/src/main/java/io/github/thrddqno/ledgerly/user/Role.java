f00fcdf819b28858a2e3ce36f530a883290baf6a
thrddqno <antonio.dioquino@hotmail.com>
2026-02-03 23:22:50 +0800

    make user implement userdetails and add role enum

---
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/user/Role.java
@@ -0,0 +1,7 @@
+package io.github.thrddqno.ledgerly.user;
+
+public enum Role {
+
+	USER,
+	ADMIN
+}
--- ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/user/User.java
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/user/User.java
@@ -6,9 +6,13 @@ import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 import org.hibernate.annotations.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import io.github.thrddqno.ledgerly.wallet.Wallet;
 
@@ -18,7 +22,7 @@ import io.github.thrddqno.ledgerly.wallet.Wallet;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,10 +42,43 @@ public class User {
 	@NotBlank
 	private String password;
 	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
 	@CreationTimestamp
 	@Column(updatable = false)
 	private LocalDateTime createdAt;
 	
-	@OneToMany(mappedBy = "user")
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
 	private List<Wallet> wallets;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+	    return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+	    return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+	    return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+	    return true;
+	}
 }
