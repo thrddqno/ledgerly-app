cb1eebcbf918ea06b51606de435a9ea6d843f323
thrddqno <antonio.dioquino@hotmail.com>
2026-02-03 23:17:23 +0800

    remove user builder in main

--- ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/LedgerlyApplication.java
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/LedgerlyApplication.java
@@ -1,13 +1,33 @@
 package io.github.thrddqno.ledgerly;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.thrddqno.ledgerly.user.User;
+import io.github.thrddqno.ledgerly.user.UserRepository;
 
 @SpringBootApplication
 public class LedgerlyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LedgerlyApplication.class, args);
+	}
+	
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(UserRepository repository) {
+		return args -> {
+			var user = User.builder()
+			.firstName("Antonio")
+			.lastName("Dioquino")
+			.email("therddioquino@hotmail.com")
+			.password("secret")
+			.build();
+			
+			repository.save(user);
+		};
 	}
 
 }
