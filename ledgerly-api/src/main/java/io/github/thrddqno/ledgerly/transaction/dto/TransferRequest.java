1609a00fd1c058eee4bb6d67d439899aeb9ddb1e
thrddqno <antonio.dioquino@hotmail.com>
2026-02-06 03:20:28 +0800

    update transactionrequest and add transferrequest dto

--- ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/transaction/dto/TransferRequest.java
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/transaction/dto/TransferRequest.java
@@ -1,9 +1,11 @@
 package io.github.thrddqno.ledgerly.transaction.dto;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record TransactionRequest(
-		Integer categoryId,
+public record TransferRequest(
+		UUID sourceWallet,
+		UUID destinationWallet,
 		String note,
 		double amount,
 		LocalDate transactionDate
