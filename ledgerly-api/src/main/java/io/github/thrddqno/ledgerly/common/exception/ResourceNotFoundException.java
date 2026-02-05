2fc3537039f2b36ff260f164e180499fdd37afc9
thrddqno <antonio.dioquino@hotmail.com>
2026-02-06 03:19:24 +0800

    add resourcenotfoundexception

---
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package io.github.thrddqno.ledgerly.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
