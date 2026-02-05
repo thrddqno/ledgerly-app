d68646742424b4f9cb85f3feb2f2c12dcc6f650e
thrddqno <antonio.dioquino@hotmail.com>
2026-02-06 03:20:49 +0800

    update transactionrepository with wallet and transfer queries

--- ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/transaction/TransactionRepository.java
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/transaction/TransactionRepository.java
@@ -1,6 +1,8 @@
 package io.github.thrddqno.ledgerly.transaction;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import io.github.thrddqno.ledgerly.category.Category;
+import io.github.thrddqno.ledgerly.wallet.Wallet;
 import jakarta.transaction.Transactional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
@@ -18,4 +21,12 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
     @Query("UPDATE Transaction t SET t.category = :finalCategory WHERE t.category IN :sourceCategories")
     int reassignTransactions(@Param("finalCategory") Category finalCategory,
                              @Param("sourceCategories") List<Category> sourceCategories);
+	
+	List<Transaction> findBySourceWallet(Wallet wallet);
+	
+	Optional<Transaction> findBySourceWalletAndPublicId(Wallet wallet, UUID transactionPublicId);
+	
+	//Optional<Transaction> findByWalletAndTransferId(Wallet wallet, UUID transferId);
+	
+	List<Transaction> findByTransferId(UUID transferId);
 }
