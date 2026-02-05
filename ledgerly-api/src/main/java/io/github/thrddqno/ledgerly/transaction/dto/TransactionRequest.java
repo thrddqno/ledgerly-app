25a88a6dcbac224a87f61d6f933ebf80ff274f05
thrddqno <antonio.dioquino@hotmail.com>
2026-02-05 22:51:27 +0800

    create transaction dto

--- ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/transaction/dto/TransactionRequest.java
+++ ledgerly-api/src/main/java/io/github/thrddqno/ledgerly/transaction/dto/TransactionRequest.java
@@ -1,5 +1,12 @@
 package io.github.thrddqno.ledgerly.transaction.dto;
 
-public class TransactionRequest {
+import io.github.thrddqno.ledgerly.category.Category;
+import io.github.thrddqno.ledgerly.wallet.Wallet;
 
+public record TransactionRequest(
+		Category category,
+		Wallet wallet,
+		String note,
+		double amount
+		) {
 }
