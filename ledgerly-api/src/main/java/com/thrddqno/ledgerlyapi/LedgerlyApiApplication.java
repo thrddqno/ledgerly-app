63fbdbe2dcc707c46e85ed41c65bfadd7427edc8
thrddqno <antonio.dioquino@hotmail.com>
2026-02-23 17:04:05 +0800

    Intialize Spring Boot application files

---
+++ ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/LedgerlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.thrddqno.ledgerlyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerlyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerlyApiApplication.class, args);
+    }
+
+}
---
+++ ledgerly-api/src/test/java/com/thrddqno/ledgerlyapi/LedgerlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrddqno.ledgerlyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerlyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
