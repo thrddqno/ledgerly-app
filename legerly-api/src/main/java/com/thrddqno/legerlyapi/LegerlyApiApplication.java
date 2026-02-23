ce509a260598eb3c609bfbabfc46338b3d1753ea
thrddqno <antonio.dioquino@hotmail.com>
2026-02-23 16:47:09 +0800

    Initial project commit

---
+++ legerly-api/src/main/java/com/thrddqno/legerlyapi/LegerlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.thrddqno.legerlyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegerlyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegerlyApiApplication.class, args);
+    }
+
+}
---
+++ legerly-api/src/test/java/com/thrddqno/legerlyapi/LegerlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrddqno.legerlyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegerlyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
