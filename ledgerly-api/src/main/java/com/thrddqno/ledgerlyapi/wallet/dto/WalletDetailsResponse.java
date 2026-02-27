8608fc428312a1d1256955be1cde831ab9cea407
thrddqno <antonio.dioquino@hotmail.com>
2026-02-27 12:35:42 +0800

    wallet: fix data type of certain fields for WalletDetailsResponse

--- ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/wallet/dto/WalletDetailsResponse.java
+++ ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/wallet/dto/WalletDetailsResponse.java
@@ -1,13 +1,14 @@
 package com.thrddqno.ledgerlyapi.wallet.dto;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record WalletDetailsResponse(
         UUID id,
         String name,
-        String startingBalance,
-        String cachedTransactions,
+        BigDecimal startingBalance,
+        BigDecimal cachedTransactions,
         LocalDateTime createdAt
 ) {
 }
