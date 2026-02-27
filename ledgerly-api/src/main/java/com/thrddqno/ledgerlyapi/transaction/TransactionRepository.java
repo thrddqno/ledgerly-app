0e5ea24b5d7b40b50b3dbf6ace3cf7fef61ffc6a
thrddqno <antonio.dioquino@hotmail.com>
2026-02-27 21:23:20 +0800

    transaction: add Keyset Pagination Query - add date range filtering - sort by most recent transaction date and by UUIDv7

--- ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/transaction/TransactionRepository.java
+++ ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/transaction/TransactionRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -24,10 +25,21 @@ public interface TransactionRepository extends JpaRepository<Transaction, UUID> 
     @Query("UPDATE Transaction tx set tx.category = :category WHERE tx.category IN :targetCategories")
     int reassignTransactions(@Param("category") Category category, @Param("targetCategories") List<Category> targetCategories);
 
-    /**
-     * KEYSET PAGINATION
-     * TODO: Create Keyset Pagination Query
-     */
+    @Query("""
+SELECT t FROM Transaction t
+WHERE t.wallet = :wallet
+  AND (CAST(:startDate AS localdate) IS NULL OR CAST(:endDate AS localdate) IS NULL OR t.date BETWEEN :startDate AND :endDate)
+  AND (CAST(:lastDate AS localdate) IS NULL OR t.date < :lastDate OR (t.date = :lastDate AND t.id < :lastId))
+ORDER BY t.date DESC, t.id DESC
+""")
+    List<Transaction> findTransactionsByWallet(
+            @Param("wallet") Wallet wallet,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("lastDate") LocalDate lastDate,
+            @Param("lastId") UUID lastId,
+            Pageable pageable
+    );
 
     //normal pagination
     Page<Transaction> findAllByWallet(Wallet wallet, Pageable pageable);
@@ -39,4 +51,5 @@ public interface TransactionRepository extends JpaRepository<Transaction, UUID> 
     void deleteAllByWallet(Wallet wallet);
 
     List<Transaction> findByTransferId(UUID transferId);
+
 }
