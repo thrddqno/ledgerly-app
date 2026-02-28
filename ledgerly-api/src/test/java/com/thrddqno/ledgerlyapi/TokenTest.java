ec1d71711e368c63c1853b309229068bab2406cc
thrddqno <antonio.dioquino@hotmail.com>
2026-02-28 16:44:32 +0800

    auth: remove unnecessary imports

--- ledgerly-api/src/test/java/com/thrddqno/ledgerlyapi/TokenTest.java
+++ ledgerly-api/src/test/java/com/thrddqno/ledgerlyapi/TokenTest.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.Cookie;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
