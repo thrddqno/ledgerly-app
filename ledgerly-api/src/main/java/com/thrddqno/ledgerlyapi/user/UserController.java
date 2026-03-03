83acbb98e5a9e84cdc3682d0a9a81802b2999822
thrddqno <antonio.dioquino@hotmail.com>
2026-03-03 19:12:08 +0800

    back-end: make getMe return a HttpStatus of unauthorized if not authenticated

--- ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/user/UserController.java
+++ ledgerly-api/src/main/java/com/thrddqno/ledgerlyapi/user/UserController.java
@@ -2,8 +2,11 @@ package com.thrddqno.ledgerlyapi.user;
 
 import com.thrddqno.ledgerlyapi.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +20,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getMe(@AuthenticationPrincipal User user){
+    public ResponseEntity<UserResponse> getMe(Authentication authentication){
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = (User) userDetails;
         return ResponseEntity.ok(userService.getMe(user));
     }
 }
