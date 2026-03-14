3d427ed529c02b0adaa4b61a3ea6836fbcf25d90
thrddqno <antonio.dioquino@hotmail.com>
2026-03-14 14:07:17 +0800

    frontend: add types and api endpoints for user

---
+++ ledgerly-web/src/domains/auth/api/auth.ts
@@ -0,0 +1,37 @@
+import api from '../../../common/api/axios.ts'
+
+export async function fetchUser(signal?: AbortSignal) {
+    const response = await api.get('/user/me', { signal })
+    return response.data
+}
+
+export async function checkAuth(signal?: AbortSignal) {
+    const response = await api.get('/user/check-auth', { signal })
+    return response.data
+}
+
+export async function loginRequest(
+    data: { email: string; password: string },
+    signal?: AbortSignal
+) {
+    const response = await api.post('/auth/login', data, { signal })
+    return response.data
+}
+
+export async function registerRequest(
+    data: {
+        firstName: string
+        lastName: string
+        email: string
+        password: string
+    },
+    signal?: AbortSignal
+) {
+    const response = await api.post('/auth/register', data, { signal })
+    return response.data
+}
+
+export async function logoutRequest(signal?: AbortSignal) {
+    const response = await api.post('/auth/logout', {}, { signal })
+    return response.data
+}
