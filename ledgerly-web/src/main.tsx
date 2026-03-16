2373607cc0df7d678978bf909cd9256c41b92e8e
thrddqno <antonio.dioquino@hotmail.com>
2026-03-16 20:24:11 +0800

    frontend: wrap App inside QueryClientProvider

--- ledgerly-web/src/main.tsx
+++ ledgerly-web/src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -5,10 +6,14 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app/App.tsx'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>
 )
