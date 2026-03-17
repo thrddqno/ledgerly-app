8050ad5e4e84c141df0d274b29a36f483a7e7983
thrddqno <antonio.dioquino@hotmail.com>
2026-03-17 20:04:32 +0800

    frontend: modify TransactionsPage.tsx to display wallet carousel

--- ledgerly-web/src/pages/TransactionsPage.tsx
+++ ledgerly-web/src/pages/TransactionsPage.tsx
@@ -1,10 +1,15 @@
+import { WalletCarousel } from '../domains/wallets/components/WalletCarousel.tsx'
+import NavBar from '../shared/ui/layout/NavBar.tsx'
 import SideBar from '../shared/ui/layout/SideBar.tsx'
 
 export default function TransactionsPage() {
     return (
-        <div className="bg-base-200">
+        <div className="bg-base-200 flex flex-row ">
             <SideBar />
-            transaction page lad
+            <div className="flex flex-col w-full h-screen overflow-hidden px-7 py-3">
+                <NavBar />
+                <WalletCarousel />
+            </div>
         </div>
     )
 }
