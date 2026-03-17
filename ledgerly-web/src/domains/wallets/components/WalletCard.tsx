c63fa9edaef036df72da62fd732842ed12a974ba
thrddqno <antonio.dioquino@hotmail.com>
2026-03-17 20:03:10 +0800

    frontend: add a hovering color effect on unselected wallets

--- ledgerly-web/src/domains/wallets/components/WalletCard.tsx
+++ ledgerly-web/src/domains/wallets/components/WalletCard.tsx
@@ -21,14 +21,14 @@ export default function WalletCard({ wallet }: Props) {
                 onClick={() => {
                     setSelectedWalletId(wallet.id)
                 }}
-                className={`cursor-pointer p-5 bg-base-100 w-full shrink-0 h-25 rounded-field border ${isSelectedWallet ? 'border-accent border-2' : 'border-base-300'}`}
+                className={`cursor-pointer transition-all p-5 bg-base-100 w-full shrink-0 h-25 rounded-field border ${isSelectedWallet ? 'border-accent border' : 'hover:bg-base-100/10 border-base-300'}`}
             >
                 <div className="flex w-full flex-row items-center gap-4">
                     <div className="flex border-accent border-2 h-10 w-10 rounded-3xl items-center justify-center">
                         <WalletIcon className="w-5 text-accent" />
                     </div>
                     <div className="flex min-w-0 flex-col items-start">
-                        <span className="text-text-primary max-tablet:text-xs max-desktop:text-sm max-phone:text-[0.75rem] truncate">
+                        <span className="text-primary max-tablet:text-xs max-desktop:text-sm max-phone:text-[0.75rem] truncate">
                             {wallet ? wallet.name : 'Wallet'}
                         </span>
                         <span
