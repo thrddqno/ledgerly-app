597a591898a83da1d74b0b2332f7337c1930275e
Antonio Dioquino <antonio.dioquino@hotmail.com>
2026-03-20 02:14:08 +0800

    frontend: add fontawesome libs for fas and far

--- ledgerly-web/src/main.tsx
+++ ledgerly-web/src/main.tsx
@@ -1,3 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -7,7 +10,7 @@ import App from './app/App.tsx'
 import './index.css'
 
 const queryClient = new QueryClient()
-
+library.add(fas, far)
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
