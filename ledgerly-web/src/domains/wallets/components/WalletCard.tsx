93003e31e2f084e561ba3bf8615c6b056465f769
Antonio Dioquino <antonio.dioquino@hotmail.com>
2026-03-19 17:05:06 +0800

    frontend: rearrange contents of Wallet Card

--- ledgerly-web/src/domains/wallets/components/WalletCard.tsx
+++ ledgerly-web/src/domains/wallets/components/WalletCard.tsx
@@ -18,26 +18,34 @@ export default function WalletCard({ wallet }: Props) {
     return (
         <div className="group relative">
             <button
-                onClick={() => {
-                    setSelectedWalletId(wallet.id)
-                }}
-                className={`cursor-pointer transition-all p-5 bg-base-100 w-full shrink-0 h-25 rounded-field border ${isSelectedWallet ? 'border-accent border' : 'hover:bg-base-100/10 border-base-300'}`}
+                onClick={
+                    isSelectedWallet
+                        ? () => setSelectedWalletId('')
+                        : () => setSelectedWalletId(wallet.id)
+                }
+                className={`cursor-pointer transition-all p-5 w-full shrink-0 h-25 rounded-field border border-base-300 ${isSelectedWallet ? 'bg-accent hover:bg-accent/85' : 'hover:bg-base-100/10 bg-base-100'}`}
             >
-                <div className="flex w-full flex-row items-center gap-4">
-                    <div className="flex border-accent border-2 h-10 w-10 rounded-3xl items-center justify-center">
-                        <WalletIcon className="w-5 text-accent" />
-                    </div>
-                    <div className="flex min-w-0 flex-col items-start">
-                        <span className="text-primary max-tablet:text-xs max-desktop:text-sm max-phone:text-[0.75rem] truncate">
-                            {wallet ? wallet.name : 'Wallet'}
-                        </span>
-                        <span
-                            className={`text-sm w-full truncate font-semibold tabular-nums ${isPositive ? 'text-success' : 'text-error'} `}
+                <div className="flex w-full flex-col items-start gap-1">
+                    <div className="flex flex-row items-center justify-start gap-3">
+                        <div
+                            className={`flex transition-all  ${isSelectedWallet ? 'bg-accent-content/10' : 'bg-base-content/10'} h-8 w-8 rounded-selector items-center justify-center`}
                         >
-                            {isPositive ? '+' : ''}
-                            {formatted}
+                            <WalletIcon
+                                className={`w-4 transition-all ${isSelectedWallet ? 'text-accent-content' : 'text-base-content'}`}
+                            />
+                        </div>
+                        <span
+                            className={`${isSelectedWallet ? 'text-accent-content' : 'text-primary'} text-md font-semibold truncate`}
+                        >
+                            {wallet.name}
                         </span>
                     </div>
+                    <span
+                        className={`text-lg truncate font-extrabold tabular-nums ${isPositive ? 'text-success' : 'text-error'} `}
+                    >
+                        {isPositive ? '+' : ''}
+                        {formatted}
+                    </span>
                 </div>
             </button>
         </div>
