d71e6ce4447fb904d779b0c6b5410a3dea24803d
Antonio Dioquino <antonio.dioquino@hotmail.com>
2026-03-23 14:37:57 +0800

    frontend: make set function able to have a union type consisting of null

--- ledgerly-web/src/domains/wallets/stores/walletStore.ts
+++ ledgerly-web/src/domains/wallets/stores/walletStore.ts
@@ -2,10 +2,10 @@ import { create } from 'zustand'
 
 interface WalletStore {
     selectedWalletId: string | null
-    setSelectedWalletId: (id: string) => void
+    setSelectedWalletId: (id: string | null) => void
 }
 
 export const useWalletStore = create<WalletStore>((set) => ({
     selectedWalletId: null,
-    setSelectedWalletId: (id: string) => set({ selectedWalletId: id }),
+    setSelectedWalletId: (id: string | null) => set({ selectedWalletId: id }),
 }))
